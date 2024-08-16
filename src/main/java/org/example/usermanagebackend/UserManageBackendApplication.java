9209f06107693bea4ef6a3bf425c6e6777258b7c
XuMeilin <2138148199@qq.com>
2024-08-16 10:57:53 +0800

    init

---
+++ src/main/java/org/example/usermanagebackend/UserManageBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.usermanagebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManageBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManageBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/usermanagebackend/UserManageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.usermanagebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManageBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
